b95027360aba42daa2b3e0269a028e9fc6393768
clark <clarkway519@gmail.com>
2020-04-20 12:55:07 +0300

    my assignment-1 phyton if clause

---
+++ assignment_1.py
@@ -0,0 +1,6 @@
+name=input("write your first name :").lower().strip()
+key_name= "clark"
+if name==key_name:
+     print(f"hello,{key_name.capitalize()}! The password is : @12")
+else:
+     print (f"Hello, {name.capitalize()}! See you later.")
