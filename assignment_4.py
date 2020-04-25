30f51448f0c78fd76efbd867b6460bb50fbb1194
clark <clarkway519@gmail.com>
2020-04-26 01:38:18 +0300

    prime number correction

--- assignment_4.py
+++ assignment_4.py
@@ -1,48 +1,22 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "print (\"if you want to quit, press '0' \")\n",
-    "while True:\n",
-    "    go_on=True\n",
-    "    given=input(\"Please enter an integer number: \")\n",
-    "    if not given.isdecimal():\n",
-    "      print(\"This is not an integer number, enter an integer number!\")\n",
-    "      go_on=False\n",
-    "    \n",
-    "    if go_on!=False:\n",
-    "      num=int(given)\n",
-    "      prime=[2,3,5,7]\n",
-    "      if num in prime:\n",
-    "        print (num, \"is a prime number\")\n",
-    "      elif num%2!=0 and num%3!=0 and num%5!=0 and num%7!=0 and num>1:\n",
-    "        print (num, \"is a prime number\")\n",
-    "      elif num==0:\n",
-    "        break\n",
-    "      else:\n",
-    "        print (num, \"is NOT a prime number\")"
-   ]
-  }
- ],
- "metadata": {
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": 3
-  },
-  "orig_nbformat": 2
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+print ("if you want to quit, press '0' ")
+while True:
+  go_on=True
+  num=input("Please enter an integer number: ")
+  if not num.isdecimal():
+    print("This is not an integer number, enter an integer number!")
+    go_on=False
+  if go_on!=False:
+    output= {
+     1:  f"{int(num)} is a prime number" ,
+     2:  f"{int(num)} is NOT a prime number"
+    }
+    total=0
+    for i in range(2, int(num)+1):
+     if int(num)%i==0:
+      total+=1
+    if total==1:
+      print(output[1])
+    elif int(num)==0:
+      break
+    else:
+      print(output[2])
