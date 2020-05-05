de8a458b693d56c6070a5f9f3e9b122abe8566f1
clark <clarkway519@gmail.com>
2020-05-05 23:43:02 +0300

    assignment_6 reload

---
+++ assignment_6.py
@@ -0,0 +1,10 @@
+n=int(input("Please enter the limit: "))
+prime=[]
+for j in range(2,n+1):
+  total=0
+  for i in range(2, j+1):
+    if j%i==0:
+      total+=1
+  if total==1:
+    prime.append(j)
+print(prime)
